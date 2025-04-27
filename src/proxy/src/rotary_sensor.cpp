0d2efa391107187c3c45b3339abd493623422291
dardengo <dardengo.cj@gmail.com>
2025-04-27 01:51:31 -0300

    🐛 Fix rotary sensor

--- src/proxy/src/rotary_sensor.cpp
+++ src/proxy/src/rotary_sensor.cpp
@@ -1,6 +1,7 @@
 #include "micras/proxy/rotary_sensor.hpp"
 #include "constants.hpp"
 #include "physics/box2d_motor.hpp"
+#include "box2d/box2d.h"
 #include <random>
 
 namespace micras::proxy {
@@ -15,16 +16,21 @@ RotarySensor::RotarySensor(const Config& config) :
 }
 
 float RotarySensor::get_position() {
-
+    float angular_velocity = 0.0f;
     if (isLeftWheel) {
         this->global_position = micrasBody->getLeftMotor().getPosition();
     } else {
         this->global_position = micrasBody->getRightMotor().getPosition();
     }
-
+    
     micrasverse::types::Vec2 delta_position = this->global_position - this->last_global_position;
     this->last_global_position = this->global_position;
-    float distance = delta_position.length();
+    
+    float distance_sign = b2Dot((b2Vec2){delta_position.x, delta_position.y}, micrasBody->linearVelocity);
+
+    float distance = std::copysignf(delta_position.length(), distance_sign);
+    
+    
     this->position += distance / micrasverse::MICRAS_WHEEL_RADIUS;
 
     return this->position;
